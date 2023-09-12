6b47a6b5ec2bed1c54fd991077069fb4dbc96b78
JamshedK <karimnazarovj@gmail.com>
2023-09-11 21:39:19 -0500

    Added a new reverse function

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def product (array):
         res = res = res * num
     return res
 
+def reverse(array):
+    return array[::-1]
+
 if __name__ == '__main__':
     array = [1,2,3,4]
     print(sum(array))
